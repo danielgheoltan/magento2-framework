ee26be4fb141aec825a1f3b8d9b71091cb5047cc
Daniel Gheoltan <daniel.gheoltan@evozon.com>
2019-01-21 09:19:52 +0200

    Updates

--- Plugin/View/Element/Html/Link/CurrentPlugin.php
+++ Plugin/View/Element/Html/Link/CurrentPlugin.php
@@ -1,14 +1,21 @@
 <?php
-namespace DG\Framework\Plugin\Element\Html\Link;
+namespace DG\Framework\Plugin\View\Element\Html\Link;
 
-class Current
+class CurrentPlugin
 {
     /**
+     * afterIsCurrent
+     *
      * @param \Magento\Framework\View\Element\Html\Link\Current $subject
-     * @return \Magento\Framework\View\Element\Html\Link\Current
+     * @param $result
+     * @return bool
      */
-    public function aroundIsCurrent(\Magento\Framework\View\Element\Html\Link\Current $subject)
+    public function afterIsCurrent(\Magento\Framework\View\Element\Html\Link\Current $subject, $result)
     {
-        return $subject;
+        $currentPageUrl = $subject->getUrl('*/*/*', ['_current' => true, '_use_rewrite' => true, '_query' => '']);
+        $href = rtrim($subject->getHref(), '/');
+
+        return ($currentPageUrl == $href) || $result;
     }
 }
+
